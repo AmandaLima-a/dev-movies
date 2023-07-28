a31b22599c2826d9a7b8b571cedaaf1800d62cbb
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-28 19:13:18 -0300

    atualizando projeto devmovies

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import Logo from '../../assets/logo.png'
+
+function Header() {
+
+    return(
+        <div>
+            <img src={Logo} alt="logo-devmovies" />
+        </div>
+    )
+}
+
+export default Header 
+
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,8 +2,8 @@ function Home(){
 
     return (
         <div>
-        <h1>Home</h1>
-        <p>Essa é a Home</p>
+            <h1>Home</h1>
+            <p>Essa é a Home</p>
         </div>
     )
 }
