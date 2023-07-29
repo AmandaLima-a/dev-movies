3b7e0e9c6e0a3f850d087183ce83aa4845500c23
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-29 20:15:52 -0300

    atualizando projeto devmovies

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,20 +1,45 @@
+import Button from '../../components/Button'
 import api from '../../services/api'
+import { Background, Info, Poster, Container, ContainerButtons } from './styles'
+import { useState, useEffect } from 'react'
 
 function Home(){
+    const [movie, setMovie] = useState()
 
-    async function getMovies() {
-        const data = await api.get('/movie/popular')
+    useEffect(() => {
+        async function getMovies() {
+            const { 
+                data: { results }
+            } = await api.get('/movie/popular')
+    
+            setMovie(results[17])
+        }
+    
+        getMovies()
 
-        console.group(data)
-    }
-
-    getMovies()
+    }, [])
+    
 
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Essa é a Home</p>
-        </div>
+        <>
+        {movie && (
+        <Background img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
+            <Container>
+                <Info>
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                    <ContainerButtons>
+                        <Button red={true}>Assista Agora</Button>
+                        <Button red={false}>Assista o Trailer</Button>
+                    </ContainerButtons>
+                </Info>
+                <Poster>
+                    <img alt="capa-do-filme" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+                </Poster>
+            </Container>
+        </Background>
+        )}
+        </>
     )
 }
 
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+    background-image: url(${ (props) => props.img});
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0; 
+        left: 0;
+        width: 100%;
+        height: 100%;
+
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+`
+
+export const Info = styled.div`
+    z-index: 2;  // pra ficar na frente do background
+    padding: 20px;
+    width: 50%;
+
+    h1 {
+        color: #FFFFFF;
+        font-size: 5rem;
+        font-weight: 700;
+    }
+
+    p {
+        font-size: 20px;
+        font-weight: 500;
+        color: #FFFFFF;
+        margin-top: 30px;
+        margin-bottom: 20px;
+    }
+`
+
+export const Poster = styled.div`
+    z-index: 3;
+    img {
+        width: 400px;
+        border-radius: 30px;
+    }
+`
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 100%;
+    max-width: 1500px;
+`
+
+export const ContainerButtons = styled.div`
+    display: flex;
+    gap: 20px;
+    margin-top: 30px;
+`
