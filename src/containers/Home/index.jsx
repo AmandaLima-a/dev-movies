0ef8c59bb8dddc208f18fc95df4c3791e0ea66c7
AmandaLima-a <amandasuelemjl@gmail.com>
2023-07-31 13:30:26 -0300

    Atualizando projeto

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,13 @@
 import Button from '../../components/Button'
+import Slider from '../../components/Slider'
 import api from '../../services/api'
+import { getImages } from '../../utils/getImages'
 import { Background, Info, Poster, Container, ContainerButtons } from './styles'
 import { useState, useEffect } from 'react'
 
 function Home(){
     const [movie, setMovie] = useState()
+    const [topMovies, setTopMovies] = useState()
 
     useEffect(() => {
         async function getMovies() {
@@ -12,33 +15,43 @@ function Home(){
                 data: { results }
             } = await api.get('/movie/popular')
     
-            setMovie(results[17])
+            setMovie(results[0])
+        }
+
+        async function getTopMovies() {
+            const { 
+                data: { results }
+            } = await api.get('/movie/top_rated')
+    
+            console.log(results)
+            setTopMovies(results)
         }
     
         getMovies()
-
+        getTopMovies()
     }, [])
     
 
     return (
         <>
         {movie && (
-        <Background img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
+        <Background img={getImages(movie.backdrop_path)}>
             <Container>
                 <Info>
                     <h1>{movie.title}</h1>
                     <p>{movie.overview}</p>
                     <ContainerButtons>
-                        <Button red={true}>Assista Agora</Button>
-                        <Button red={false}>Assista o Trailer</Button>
+                        <Button red>Assista Agora</Button>
+                        <Button>Assista o Trailer</Button>
                     </ContainerButtons>
                 </Info>
                 <Poster>
-                    <img alt="capa-do-filme" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+                    <img alt="capa-do-filme" src={getImages(movie.poster_path)}/>
                 </Poster>
             </Container>
         </Background>
         )}
+        {topMovies && <Slider info={topMovies} title={'Top Filmes'} />}
         </>
     )
 }
